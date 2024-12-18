c3fb5d4cad97f31d99bf85d632656abe34d3628d
Charaka Gunasinghe <charaka.info@gmail.com>
2024-12-18 15:42:48 +0530

    section 01 - getting started completed

--- Vidly/obj/Debug/net8.0/Vidly.AssemblyInfo.cs
+++ Vidly/obj/Debug/net8.0/Vidly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vidly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab159b1af03a4fbd1be8a7425c4057959fbcddb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vidly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vidly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
